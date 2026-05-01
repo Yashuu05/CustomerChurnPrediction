b7dd3220c87a94f1bc81f38e58f70175b2376f16
Yashuu05 <chillalyash2005@gmail.com>
2026-05-01 19:30:03 +0530

    initialized project

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='churn_prediction_package',
+    version='0.0.1',
+    description='A Python package',
+    author='Yash Chillal',
+    author_email='chillalyash2005@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn',
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'xgboost',
+        'mlflow'
+    ],
+)
