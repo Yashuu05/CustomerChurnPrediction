f3b65cf01ade3d87026943ce859a476097837bcb
Yashuu05 <chillalyash2005@gmail.com>
2026-05-03 23:17:46 +0530

    Created model utilities

---
+++ utils/model_utils.py
@@ -0,0 +1,48 @@
+import joblib as jb
+from src.logger import logging
+
+#load model
+def load_model(file_path):
+    try:
+        return jb.load(file_path)
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        print(f"Error! {str(e)}")
+        return None
+    
+# save model
+def save_model(file_path, model):
+    try:
+        jb.dump(value=model, filename=file_path)
+        return f"Successul! Model saved."
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        print(f"Error! {str(e)}")
+        return None
+    
+# evaluating model's perfromance for supervised model
+def evalulate_model(y_test, y_pred):
+    """
+    this function evaluates' performance of trained supervised model
+    Input: y_test, y_pred 
+    Output: list of recall, precision, f1 socre, accuracy, roc_auc_score and classification report
+    """
+    
+    from sklearn.metrics import  accuracy_score, precision_score, recall_score, f1_score
+    try:
+        # 1
+        recall = recall_score(y_test, y_pred, average="weighted")
+        # 2
+        precision = precision_score(y_test, y_pred, average="weighted")
+        # 3
+        f1 = f1_score(y_test, y_pred, average="weighted")
+        # 4
+        acc = accuracy_score(y_test, y_pred)
+
+        results = [recall, precision, f1, acc]
+        
+        return results
+    
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        print(f"Error! {str(e)}")
